14d08f1809ff9a0e74fe907f0fd439bcb6ccc845
justinouanko <justouanko@gmail.com>
2026-05-19 23:01:30 +0000

    daily

--- Week1/Day1/DailyChallenge/daily_challenge.py
+++ Week1/Day1/DailyChallenge/daily_challenge.py
@@ -5,30 +5,24 @@ number = int(input())
 print("Entrez la longeur du nombre en chiffre: ")
 length = int(input())
 
-multiples_list = []
+
+multiples = []
 
 for i in range(1, length + 1):
-    # Calculate the multiple and add it to the list
-    multiples_list.append(number * i)
+    multiples.append(number * i)
 
-print(f"number: {number} - length {length} ➞ {multiples_list}")
+print(multiples)
 
 
 
 # CHALLENGE 2
 
+word = input("Entrez un mot : ")
 
-# 1. Demander les entrées à l'utilisateur et les convertir en entiers (int)
-number = int(input("Entrez le nombre de base : "))
-length = int(input("Entrez la longueur désirée pour la liste : "))
+result = ""
 
-# 2. Initialiser une liste vide pour stocker les multiples
-multiples_list = []
+for i in range(len(word)):
+    if i == 0 or word[i] != word[i - 1]:
+        result += word[i]
 
-# 3. Boucler de 1 jusqu'à (length + 1) car la fonction range() exclut la dernière valeur
-for i in range(1, length + 1):
-    # Calculer le multiple et l'ajouter à la liste
-    multiples_list.append(number * i)
-
-# 4. Afficher le résultat final
-print(f"nombre : {number} - longueur {length} ➞ {multiples_list}")
+print(result)
